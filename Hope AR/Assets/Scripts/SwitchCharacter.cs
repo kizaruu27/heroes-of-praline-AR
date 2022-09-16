ab92c86ccb2ab4bbdf8c278cda9ecd15cc301a86
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2022-09-16 14:26:27 +0700

    add: switch character

---
+++ Hope AR/Assets/Scripts/SwitchCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCharacter : MonoBehaviour
+{
+    public GameObject[] characters;
+    [SerializeField] private int charIndex;
+
+    public void OnClickSwitchCharacter()
+    {
+        characters[charIndex].SetActive(false);
+        charIndex = (charIndex + 1) % characters.Length;
+        characters[charIndex].SetActive(true);
+    }
+}
