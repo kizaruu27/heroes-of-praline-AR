947d6dcd80609294f4a57ea3372cc716b589d6c3
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2022-09-21 15:15:04 +0700

    double sided shader

---
+++ Hope AR/Assets/Scripts/FlipMesh.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FlipMesh : MonoBehaviour
+{
+   private Mesh _mesh;
+
+   private void Start()
+   {
+      _mesh = GetComponent<MeshFilter>().sharedMesh;
+   }
+}
